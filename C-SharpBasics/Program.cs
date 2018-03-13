60ac8be48c4f5d391b96b578e229c86a0c92655c
Colin Stevens <mail@colinjstevens.com>
2018-03-13 16:02:56 -0500

    Initial

---
+++ C-SharpBasics/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_SharpBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey(); // Prevent console window from auto closing.
+        }
+    }
+}
